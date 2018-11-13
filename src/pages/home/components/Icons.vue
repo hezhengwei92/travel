9e6acf3be1475d38731fcb9c2ea78dc2519cb0dd
hezhengwei92 <419084526@qq.com>
2018-11-13 17:59:50 +0800

    轮播

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="icons">
+      <div class="icon">
+        <div class="icon-img">
+          <img  class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
+        </div>
+        <p>热门景点</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+<!-- 设定固定宽高比 因为 padding-bottom 是根据with 来计算的 -->
+<style lang="stylus" scoped>
+  .icons
+    overflow: hidden
+    height: 0
+    padding-bottom: 50%
+    background: green
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      padding-bottom: 25%
+      background: red
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        background:blue
+        .icon-imgcontent
+          height :100%
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption"  v-if="showSwiper">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.igmUrl"></swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        igmUrl: 'http://tupian.qqjay.com/u/2017/1118/1_162252_2.jpg'
+      }, {
+        id: '0002',
+        igmUrl: 'http://tupian.qqjay.com/u/2017/1118/1_162252_8.jpg'
+      }]
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.swiperList.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
