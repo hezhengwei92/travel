8424406ae7f239a215a0f8b25db33d9485162d2e
hezhengwei92 <419084526@qq.com>
2018-11-15 23:20:18 +0800

    图片轮播生效

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption"  v-if="showSwiper">
+    <swiper  v-if="showSwiper">
       <swiper-slide v-for="item of swiperList" :key="item.id">
-        <img class="swiper-img" :src="item.igmUrl"></swiper-slide>
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
   </div>
@@ -13,16 +14,15 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {
-        pagination: '.swiper-pagination',
-        loop: true
-      },
       swiperList: [{
         id: '0001',
-        igmUrl: 'http://tupian.qqjay.com/u/2017/1118/1_162252_2.jpg'
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/fe/fd9c295c0e1a7602.jpg_750x200_11c8343f.jpg'
       }, {
         id: '0002',
-        igmUrl: 'http://tupian.qqjay.com/u/2017/1118/1_162252_8.jpg'
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/31/da037478f37cf202.jpg_750x200_fe28d396.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/68/c66e4cde1ee62602.jpg_750x200_e9b97acd.jpg'
       }]
     }
   },
